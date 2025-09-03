9d533b5760e77931757557342e97983ecd594add
sunnyychung <124360737+sunnyychung@users.noreply.github.com>
2025-09-03 01:10:01 +0100

    Initial commit

---
+++ webpack.dev.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: "./src/template.html"
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ]
+    }
+}
